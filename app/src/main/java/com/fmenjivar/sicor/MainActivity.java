2ec7994842f35a0b11ad899b831197cdee52242f
menjivar98 <37199230+menjivar98@users.noreply.github.com>
2019-05-14 23:31:47 -0600

    Se agrego la actividad de registro y se completo la funcionalidad de login

--- app/src/main/java/com/fmenjivar/sicor/MainActivity.java
+++ app/src/main/java/com/fmenjivar/sicor/MainActivity.java
@@ -2,7 +2,12 @@ package com.fmenjivar.sicor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.fmenjivar.sicor.Activities.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Primer paso
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null){
+
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+        }
+
+
     }
 }
