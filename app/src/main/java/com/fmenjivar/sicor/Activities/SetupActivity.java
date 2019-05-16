e17ac003d63461a4d648b7227e1908d69f26c11e
Fernando Menjivar <00195316@uca.edu,sv>
2019-05-15 22:04:34 -0600

    Se agrego a la actividad setup un bottom y la funcion de poder poster su imagen de perfil y su nombre

--- app/src/main/java/com/fmenjivar/sicor/Activities/SetupActivity.java
+++ app/src/main/java/com/fmenjivar/sicor/Activities/SetupActivity.java
@@ -1,5 +1,6 @@
 package com.fmenjivar.sicor.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,11 +14,25 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.fmenjivar.sicor.MainActivity;
 import com.fmenjivar.sicor.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -25,9 +40,12 @@ public class SetupActivity extends AppCompatActivity {
 
     Toolbar setupToolbar;
     private CircleImageView setupImage;
-    static int PReqCode = 1;
-    static int REQUESCODE = 1;
     private  Uri mainImageURI = null;
+    private EditText setupName;
+    private Button setupBtn;
+    private StorageReference storageReference;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +58,13 @@ public class SetupActivity extends AppCompatActivity {
 
 
         setupImage = findViewById(R.id.setup_image);
+        setupName = findViewById(R.id.setup_text);
+        setupBtn = findViewById(R.id.button_setup);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
 
         setupImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,28 +72,49 @@ public class SetupActivity extends AppCompatActivity {
 
                 if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
                     checkAndRequesForPermission();
-
-
+                }else{
+                    BringImagePicker();
                 }
+
+            }
+        });
+
+        setupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String user_name = setupName.getText().toString();
+
+                if(!user_name.isEmpty() && mainImageURI != null){
+                    setupUser(user_name);
+                }
+
             }
         });
 
     }
+
+
 
     private void checkAndRequesForPermission() {
 
         if (ContextCompat.checkSelfPermission(SetupActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(SetupActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
         }else {
-            CropImage.activity()
-                    .setGuidelines(CropImageView.Guidelines.ON)
-                    .setAspectRatio(1,1)
-                    .start(SetupActivity.this);
+
+            BringImagePicker();
         }
 
 
     }
 
+    private void BringImagePicker() {
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setAspectRatio(1,1)
+                .start(SetupActivity.this);
+
+    }
 
 
     @Override
@@ -82,5 +128,63 @@ public class SetupActivity extends AppCompatActivity {
                 Exception error = result.getError();
             }
         }
+    }
+
+    private void setupUser(final String userName) {
+
+        final String user_id = firebaseAuth.getCurrentUser().getUid();
+
+        final StorageReference image_path = storageReference.child("profile_images").child(user_id + " .jpg");
+        image_path.putFile(mainImageURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                image_path.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+
+                        Map<String, String> userMap = new HashMap<>();
+                        userMap.put("name",userName);
+                        userMap.put("image",uri.toString());
+
+                        firebaseFirestore.collection("Users").document(user_id).set(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                               if (task.isSuccessful()){
+                                   showMessage("The user setting are updated");
+                                   Intent mainIntent = new Intent(SetupActivity.this, MainActivity.class);
+                                   startActivity(mainIntent);
+                                   finish();
+
+                               }else {
+                                   showMessageErro(task.getException().getMessage());
+                               }
+
+
+
+
+                            }
+                        });
+
+
+                    }
+                });
+
+
+            }
+        });
+
+
+    }
+
+
+    private void showMessage(String text) {
+        Toast.makeText(getApplicationContext(), text,Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void showMessageErro(String text){
+        Toast.makeText(getApplicationContext(),"Error:" + text,Toast.LENGTH_SHORT).show();
     }
 }
