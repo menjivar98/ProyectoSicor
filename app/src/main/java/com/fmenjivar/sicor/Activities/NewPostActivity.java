89af77f50e9310888e3bb693a488edf103bfe124
Fernando Menjivar <00195316@uca.edu,sv>
2019-05-29 21:18:11 -0600

    Agregando la actividad new_post

---
+++ app/src/main/java/com/fmenjivar/sicor/Activities/NewPostActivity.java
@@ -0,0 +1,16 @@
+package com.fmenjivar.sicor.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fmenjivar.sicor.R;
+
+public class NewPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+    }
+}
