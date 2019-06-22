b42e26249008b9ce44320d13d00fe46812863adc
Fredy Sanchez <00082817@uca.edu.sv>
2019-06-21 21:41:52 -0600

    Fragmento Account configurado

--- app/src/main/java/com/fmenjivar/sicor/fragment/AccountFragment.java
+++ app/src/main/java/com/fmenjivar/sicor/fragment/AccountFragment.java
@@ -1,15 +1,22 @@
 package com.fmenjivar.sicor.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.fmenjivar.sicor.MainActivity;
 import com.fmenjivar.sicor.R;
+import com.fmenjivar.sicor.activities.LoginActivity;
+import com.fmenjivar.sicor.activities.SetupActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,12 +28,50 @@ public class AccountFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private FirebaseAuth firebaseAuth;
+    private  FirebaseAuth mAuth;
+    private Button buttonSettings;
+    private Button buttonlogOut;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        View v = inflater.inflate(R.layout.fragment_account, container, false);
+        v.findViewById(R.id.btnAccountSett).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent settingIntent = new Intent(getActivity(), SetupActivity.class);
+                startActivity(settingIntent);
+            }
+        });
+
+        v.findViewById(R.id.btnLogut).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logOut();
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    private void logOut(){
+        mAuth.signOut();
+        sendtoLogin();
+    }
+
+    private void sendtoLogin() {
+        Intent loginIntent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(loginIntent);
     }
 
 }
