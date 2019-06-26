9eb0ffd08e398c04a67e9ca5fdead2b746d531df
Fernando Menjivar <00195316@uca.edu,sv>
2019-06-25 22:12:52 -0600

    logo

--- app/src/main/java/com/fmenjivar/sicor/fragment/HomeFragment.java
+++ app/src/main/java/com/fmenjivar/sicor/fragment/HomeFragment.java
@@ -79,7 +79,6 @@ public class HomeFragment extends Fragment {
 
        firebaseAuth = FirebaseAuth.getInstance();
 
-
        if (firebaseAuth.getCurrentUser() != null){
            firebaseFirestore = FirebaseFirestore.getInstance();
 
@@ -103,7 +102,7 @@ public class HomeFragment extends Fragment {
                }
            });*/
 
-           Query firstQuery = firebaseFirestore.collection("Post").orderBy("timestamp",Query.Direction.DESCENDING).limit(3);
+           Query firstQuery = firebaseFirestore.collection("Post").orderBy("timestamp",Query.Direction.DESCENDING);
 
            firstQuery.addSnapshotListener(getActivity(),new EventListener<QuerySnapshot>() {
                @Override
